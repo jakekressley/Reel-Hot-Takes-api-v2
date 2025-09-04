cdbd15235c03eef04343cd775c81b546b8466b1a
jakekressley <kressleyjake@gmail.com>
2025-09-04 01:57:19 -0400

    api cors allowing

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from scraper import scrape_user
 from scoring import calculate_hotness
 
 app = FastAPI(title="Reel Hot Takes API")
+
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173/",
+    "https://reelhottakes.xyz",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
